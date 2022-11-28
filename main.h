93430bd7fa7a680eebaa98229aa9310b8a9a5e0b
Joshua April <Joshua.April@hotmail.com>
2022-11-29 11:07:36 +1300

    Create main.h

---
+++ main.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#define NBUF 16
+#define RAMSIZE 512
+#define DIRSIZ 14
+
+#define B_BUSY 1
+#define B_DIRTY 4
+#define B_VALID 2
+
+#define LOGSIZE 10
+#define MAXDEVICE 2
+#define NINODE 50
+
+#define NDIRECT 12
+
+#define I_BUSY 1
+#define I_VALID 2
+
+#define T_DIR 1
+#define T_FILE 2
+#define T_DEV 3
+
+typedef unsigned int uint;
+
+
